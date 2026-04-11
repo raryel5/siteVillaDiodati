1ae1ea4a1759dec65e987e73499e891afb3b5108
Renan Aryel <renanaryel@gmail.com>
2026-04-11 20:04:28 -0300

    update livro do Israel

--- resources/views/menus/catalogo.blade.php
+++ resources/views/menus/catalogo.blade.php
@@ -121,14 +121,14 @@ CAPAS DOS OUTROS LIVROS
 
         <div class="item">
             <div class="produto">
-                <a href="#">
+                <a href="{{ route('livros', $id=8) }}">
                     <img src="{{ Storage::url('capas/capaDRVP.jpeg') }}">
                 </a>
             </div>
 
             <h1>Dedo em Riste, Verso em Prosa</h1>
             <h2>Israel de Oliveira Costa, AAAA</h2>
-            <span>R$ XX,XX</span>
+            <span>R$ 60,00</span>
             <br>
             <a href="https://wa.me/558699203283?text=Olá,%20gostaria%20de%20adquirir%20seu%20livro:%20Dedo%20em%20Riste,%20Verso%20em%20Prosa.%20Vim%20pelo%20site%20de%20vocês.">
                 <button><?php echo $botao ?></button>
