538956a7bfa8de462d421bab439b1400331ba8cb
Renan Aryel <renanaryel@gmail.com>
2026-09-02 21:12:24 -0300

    update clientes index

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -1,6 +1,6 @@
 <!-- as configurações de página estão layouts/app -->
 @extends('layouts.app')  
-@section('title', 'Clientes')  
+@section('title', 'Apoiadores')  
 
 <!-- corpo da página -->
 @section('main')
@@ -17,7 +17,9 @@
     <tr>
       <th scope="col">#</th>
       <th scope="col">Nome</th>
+      <th scope="col">Sobrenome</th>
       <th scope="col">Valor R$</th>
+      <th scope="col">E-mail</th>
       <th scope="col">Status de pagamento</th>
       {{-- <th scope="col">Recebedor</th> --}}
     </tr>
@@ -28,7 +30,9 @@
     <tr>
       <td>{{ $cliente->id }}</td>
       <td>{{ $cliente->firstname }}</td>
+      <td>{{ $cliente->surname }}</td>
       <td>{{ $cliente->valor }}</td>
+      <td>{{ $cliente->email }}</td>
       <td>{{ $cliente->payment_status }}</td>
       {{-- <td>{{ $cliente->nameReceiver }}</td> --}}
     </tr>
